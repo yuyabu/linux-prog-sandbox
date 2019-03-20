f7968a41a665474168aad83cac96abe5b9be63ca
yuyabu <yuyabushita@gmail.com>
2019-03-20 13:49:28 +0900

    refactering

--- chap5/cat.c
+++ chap5/cat.c
@@ -9,12 +9,11 @@ static void do_cat(const char *path);
 static void die(const char *s);
 
 int main(int argc, char *argv[]){
-	int i;
 	if (argc < 2){
 		fprintf(stderr, "%s:file name not given\n", argv[0]);
 		exit(1);
 	}
-	for (i = 1; i< argc;i++){
+	for (int i = 1; i< argc;i++){
 		do_cat(argv[i]);
 	}
 	exit(0);
@@ -23,18 +22,19 @@ int main(int argc, char *argv[]){
 #define BUFFER_SIZE 2048
 
 static void do_cat(const char *path){
-	int fd;
-	unsigned char buf[BUFFER_SIZE];
-	int n;
 
-	fd = open(path, O_RDONLY);
+	int fd = open(path, O_RDONLY);
 
 	if(fd < 0) die(path);
+	
+	unsigned char buf[BUFFER_SIZE];
+	int readed_bytes;
+	
 	for(;;){
-		n = read(fd, buf,sizeof buf);
-		if ( n< 0) die(path);
-		if (n ==0) break;
-		if(write(STDOUT_FILENO, buf, n) < 0) die(path);
+		readed_bytes = read(fd, buf,sizeof buf);
+		if ( readed_bytes< 0) die(path);
+		if (readed_bytes ==0) break;
+		if(write(STDOUT_FILENO, buf, readed_bytes) < 0) die(path);
 	}
 	if(close(fd) <0) die(path);
 }
