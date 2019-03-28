61fc001298128271dc456df8739f022d2b77e653
yuyabu <yuyabushita@gmail.com>
2019-03-28 16:50:48 +0900

    練習問題5.8回答

--- chap5/rensyu1.c
+++ chap5/rensyu1.c
@@ -8,14 +8,14 @@
 #include<sys/stat.h>
 #include<fcntl.h>
 
+static void do_stdio_echo();
 static void do_cat(const char *path);
 static void die(const char *s);
 
 int main(int argc, char *argv[]){
 
 	if (argc < 2){
-		fprintf(stderr, "%s:file name not given\n", argv[0]);
-		exit(1);
+		do_stdio_echo();
 	}else{
 
 		for (int i = 1; i< argc;i++){
@@ -26,7 +26,21 @@ int main(int argc, char *argv[]){
 }
 
 #define BUFFER_SIZE 2048
-
+static void do_stdio_echo(){
+	//stdioを1回読んで出力して終わり。
+	
+	int fd = STDIN_FILENO;
+	
+	unsigned char buf[BUFFER_SIZE];
+	int readed_bytes;
+	
+	for(;;){
+		readed_bytes = read(fd, buf,sizeof buf);
+		if (readed_bytes ==0) break;
+		if(write(STDOUT_FILENO, buf, readed_bytes) < 0) die("stdin");
+	}
+	
+}
 static void do_cat(const char *path){
 
 	int fd = open(path, O_RDONLY);
@@ -44,6 +58,7 @@ static void do_cat(const char *path){
 	}
 	if(close(fd) <0) die(path);
 }
+
 
 static void die(const char *s){
 	perror(s);
