94b7547745f8ca967b1ff647d7eb55676d731bdf
yuyabu <yuyabushita@gmail.com>
2019-04-02 13:43:43 +0900

    Add getopt verification code
    It does not work well because it is just the sample code on book.

---
+++ chap7/getopt.c
@@ -0,0 +1,28 @@
+#include<unistd.h>
+#include<stdio.h>
+
+int main(int argc, char *argv[]){
+	
+	int opt;
+
+	while((opt = getopt(argc, argv, "af:tx")) != -1){
+		
+		switch (opt) {
+
+			case 'a':
+				//オプションaを処理するコード
+				printf("arg a\n");
+				break;
+			case 'f':
+				//オプション-fを処理するコード
+				printf("arg f\n");
+				break;
+			case '?':
+				printf("arg ?\n");
+				//不正なオプションを渡された場合のコード
+				break;
+		}
+
+	}
+	printf("program finished\n");
+}
