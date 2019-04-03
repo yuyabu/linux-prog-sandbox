f471d450fe05889dea7b30684d699d633fb13ccc
yuyabu <yuyabushita@gmail.com>
2019-04-03 21:59:31 +0900

    fix head.c : use long option

--- chap7/head.c
+++ chap7/head.c
@@ -1,38 +1,58 @@
 #include<stdio.h>
 #include<stdlib.h>
+#define _GNU_SOURCE
+#include<getopt.h>
 
 static void do_head(FILE *f,long nlines);
 
+#define DEFAULT_N_LINES 10
+
+static struct option longopts[]= {
+		{"lines", required_argument, NULL, 'n'},
+		{"help", no_argument, NULL,'h'},
+		{0,0,0,0}
+};
+
 int 
 main(int argc, char *argv[]){
-	long nlines;
+	long nlines=DEFAULT_N_LINES;
+	int opt;
 
-	if (argc < 2){
-		fprintf(stderr,"Usage: %s n [file file ...]\n", argv[0]);
-		exit(1);
+	while((opt = getopt_long(argc,argv,"n:",longopts,NULL)) != -1){
+		switch(opt){
+			case'n':
+				nlines= atol(optarg);
+				break;
+			case 'h':
+				fprintf(stdout,"Usage: %s [-n LINES] [FILE ...]\n",argv[0]);
+				exit(0);
+			case '?':
+				fprintf(stderr, "Usage: %s [-n LINES] [FILE ...]\n", argv[0]);
+				exit(1);
+		}
+
 	}
-	nlines = atoi(argv[1]);
-	if(argc ==2){
-		do_head(stdin, atoi(argv[1]));
-	} else {
+	
+	if(optind ==argc){
+		do_head(stdin,nlines);
+	}else{
 		int i;
-		
-		for(i = 2; i< argc; i++){
+		for(i = optind; i< argc; i++){
 			FILE *f;
+		
 			f = fopen(argv[i],"r");
 			if(!f){
 				perror(argv[i]);
 				exit(1);
 			}
-			
 			do_head(f,nlines);
 			fclose(f);
-
-
 		}
 	}
 	exit(0);
 }
+
+
 static void
 do_head(FILE *f, long nlines){
 	int c;
