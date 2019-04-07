2673ef4401f84cb5374226339ef8eb35e2827bf2
yuyabu <yuyabushita@gmail.com>
2019-04-07 23:39:20 +0900

    fix bug code (SIGFPE)

--- chap7/bug.c
+++ chap7/bug.c
@@ -1,7 +1,14 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 int main(int argc,char *argv[]){
-	if(argc < 3){
-		fprintf(stderr,"need more than 2 args");
-	}	
+	if(argc != 3){
+		fprintf(stderr,"need 2 args\n");
+	}
+	int dividend = atoi(argv[1]);
+	int divisor = atoi(argv[2]);
+	
+	float result = dividend / divisor;
+	printf("%d / % d is %f\n", dividend,divisor,result);
+		
 }
